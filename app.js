b505aa680694658334253a85e52257ce4d1d3610
dianepj12@gmail.com <dianepj12@gmail.com>
2022-01-05 22:15:50 -0600

    wee

--- app.js
+++ app.js
@@ -40,21 +40,24 @@
 //   </html>
 //   `;
 // };
+// ope
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs =process.argv.slice(2);
-
-console.log(profileDataArgs);
-
-const [name, github] = profileDataArgs;
-
-console.log(name,github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err =>{
-  if(err)throw err;
-  console.log('Portfolio complete! Check out index.html to see the output');
-});
+// fs.writeFile('./index.html', pageHTML, err =>{
+//   if(err)throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output');
+// });
+console.log(inquirer);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message:'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
